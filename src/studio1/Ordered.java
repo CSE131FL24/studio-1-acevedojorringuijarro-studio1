612290621e73f9aa7633aefe8f6834992846080f
lauren <lauren@DESKTOP-I33DL7A.dhcp.wustl.edu>
2024-09-04 15:27:00 -0500

    hi push

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -8,6 +8,7 @@ public class Ordered {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
 		Scanner in = new Scanner(System.in);
 		System.out.println("Value for x?");
 		int x = in.nextInt();
@@ -15,7 +16,9 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		
+		boolean isOrdered = (z > y) && (y > x) || (x > y) && (y > z);
+		System.out.println(isOrdered); 
 	}
 
 }
