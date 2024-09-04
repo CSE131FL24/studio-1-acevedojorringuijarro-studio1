7d1c31c56051cc31c737f10bfbb71b85748765cb
lauren <lauren@DESKTOP-I33DL7A.dhcp.wustl.edu>
2024-09-04 15:13:34 -0500

    hi pushed

--- src/studio1/HiFour.java
+++ src/studio1/HiFour.java
@@ -9,14 +9,14 @@ import java.util.Scanner;
 public class HiFour {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter value for arg 0: ");
-		String s0 = in.nextLine();
-		System.out.print("Enter value for arg 1: ");
+		String s0 = in.nextLine(); 				//always introduce the variable before using it
+		System.out.print("Hi " + s0 + ". How are you?");
 		String s1 = in.nextLine();
-		System.out.print("Enter value for arg 2: ");
+		System.out.print("Hi " + s1 + ". How are you?");
 		String s2 = in.nextLine();
-		System.out.print("Enter value for arg 3: ");
+		System.out.print("Hi " + s2 + ". How are you?");
 		String s3 = in.nextLine();
+		System.out.print("Hi " + s3 + ". How are you?");
 		//
 		// Say hello to the names in s0 through s3.
 		//
