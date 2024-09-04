fcb4894039e00515d1f0bbb641e92d14f1b46ec1
lauren <lauren@DESKTOP-I33DL7A.dhcp.wustl.edu>
2024-09-04 15:01:58 -0500

    pushed

--- src/studio1/HiOne.java
+++ src/studio1/HiOne.java
@@ -8,15 +8,18 @@ public class HiOne {
 	 * Says hello to the supplied argument
 	 */
 	public static void main(String[] args) {
-
-		Scanner in = new Scanner(System.in);
-		System.out.print("Enter value for arg 0: ");
+		
+		Scanner in = new Scanner(System.in); 
 		String name = in.nextLine();
-		//
+		System.out.println("Hi " + name + ". How are you?");
+		
 		// Below this line, enter code so that this program's output says
 		//      Hi, Pat.  How are you?
 		// if the value of name is "Pat"
-		//
+		
+		Scanner intwo = new Scanner(System.in); 
+		String nametwo = intwo.nextLine();
+		System.out.print("Hi " + nametwo + ". How are you?");
 
 	}
 
